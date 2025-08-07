ad7b1d685e8a57b397d699fd7450daebfd8c8d4b
z1nl0x <paulo.kreft@gmail.com>
2025-08-07 17:19:31 -0300

    Updates

--- bookly/src/db/main.py
+++ bookly/src/db/main.py
@@ -1,14 +1,18 @@
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel import create_engine, text, SQLModel
 from src.config import Config
 
+
 # Cria o engine assíncrono corretamente
-engine = create_async_engine(
-    Config.DATABASE_URL,
-    echo=True
+engine = AsyncEngine(
+    create_engine(
+        Config.DATABASE_URL,
+        echo=True
+    )
 )
 
 async def init_db():
     async with engine.begin() as conn:
-        result = await conn.execute(text("SELECT 'hello';"))
-        print(result.all())
+       from src.books.models import Book
+       
+       await conn.run_sync(SQLModel.metadata.create_all) 
